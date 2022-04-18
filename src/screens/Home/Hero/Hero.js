c36c56ac87d951a2586110bd70f332aea5d12adb
Angel Vazurio <angelCL0@outlook.com>
2022-04-17 23:29:41 -0400

    fixe

--- src/screens/Home/Hero/Hero.js
+++ src/screens/Home/Hero/Hero.js
@@ -109,8 +109,6 @@ export default function Hero() {
               onClick={openModal}
             >
               <svg
-                width='24'
-                height='24'
                 viewBox='0 0 24 24'
                 fill='none'
                 xmlns='http://www.w3.org/2000/svg'
