cd63aabcb3c29b000922a5c3ba9e77ad15d1c549
Angel Vazurio <angelCL0@outlook.com>
2022-04-17 23:53:55 -0400

    fox

--- src/screens/Home/Hero/Hero.js
+++ src/screens/Home/Hero/Hero.js
@@ -109,6 +109,8 @@ export default function Hero() {
               onClick={openModal}
             >
               <svg
+                width='44'
+                height='44'
                 viewBox='0 0 24 24'
                 fill='none'
                 xmlns='http://www.w3.org/2000/svg'
