4f7f729f8a96a2be947a05c838265407410f4d52
Angel Vazurio <angelCL0@outlook.com>
2022-04-20 00:10:22 -0400

    animate

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,32 @@
 import styles from './card.module.css';
 import cn from 'classnames';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
+
+const variants = {
+  visible: { opacity: 1, transition: { staggerChildren: 0.2 } },
+  hidden: { opacity: 0 },
+};
 
 const Card = (props) => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start('visible');
+    }
+  }, [controls, inView]);
+
   return (
-    <article className={styles.container}>
+    <motion.article
+      variants={variants}
+      initial='hidden'
+      ref={ref}
+      animate={controls}
+      className={styles.container}
+    >
       <div className={styles.image_container}>
         <img className={styles.image} src={props.image} />
       </div>
@@ -12,7 +35,7 @@ const Card = (props) => {
         <h3 className={cn('body-2-bold', styles.title)}>{props.title}</h3>
         <p className={cn('caption-2')}>{props.date}</p>
       </div>
-    </article>
+    </motion.article>
   );
 };
 
--- src/screens/Home/Gallery/Gallery.js
+++ src/screens/Home/Gallery/Gallery.js
@@ -1,6 +1,24 @@
 import cn from 'classnames';
 import Card from '../../../components/Card';
 import styles from './gallery.module.css';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
+
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const item = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
 
 const gallery = [
   {
@@ -16,13 +34,58 @@ const gallery = [
   {
     image: 'images/03.jpg',
     title: 'Plants',
+    date: 'April 17, 2022',
+  },
+  {
+    image: 'images/04.jpg',
+    title: 'Sea',
+    date: 'April 15, 2022',
+  },
+  {
+    image: 'images/05.jpg',
+    title: 'Spian',
+    date: 'April 14, 2022',
+  },
+  {
+    image: 'images/06.jpg',
+    title: 'Interior',
+    date: 'April 12, 2022',
+  },
+  {
+    image: 'images/07.jpg',
+    title: 'Triangle',
     date: 'April 10, 2022',
+  },
+  {
+    image: 'images/08.jpg',
+    title: 'Paint',
+    date: 'April 09, 2022',
+  },
+  {
+    image: 'images/09.jpg',
+    title: 'Autumn',
+    date: 'April 09, 2022',
+  },
+  {
+    image: 'images/10.jpg',
+    title: 'Squares',
+    date: 'April 06, 2022',
+  },
+  {
+    image: 'images/11.jpg',
+    title: 'Mountains',
+    date: 'April 03, 2022',
+  },
+  {
+    image: 'images/12.jpg',
+    title: 'Dark Sea',
+    date: 'April 02, 2022',
   },
 ];
 
 export default function Gallery() {
   return (
-    <section className={cn('section section-pb')}>
+    <motion.section className={cn('section section-pb')}>
       <div className={cn('container')}>
         <h1 className={cn('h4')}>Tabs</h1>
 
@@ -32,6 +95,6 @@ export default function Gallery() {
           ))}
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 }
