b945fdf83abc25ae22a41734de164f827bbc761e
“Angel <angelCL0@outlook.com>
2022-05-27 11:34:41 -0400

    fixed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import './App.css';
-import './styles/app.css';
-import { Routes, Route, BrowserRouter, Link } from 'react-router-dom';
-import Home from './screens/Home';
-import Page from './components/Page';
-import HomeV2 from './screens/HomeV2';
+import "./App.css";
+import "./styles/app.css";
+import { Routes, Route, BrowserRouter, Link } from "react-router-dom";
+import Home from "./screens/Home";
+import Page from "./components/Page";
+import HomeV2 from "./screens/HomeV2";
+import HomeV3 from "./screens/HomeV3";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route
-          path='/'
+          path="/"
           element={
             <Page>
               <Home />
@@ -18,10 +19,18 @@ function App() {
           }
         />
         <Route
-          path='/homev2'
+          path="/homev2"
           element={
             <Page>
               <HomeV2 />
+            </Page>
+          }
+        />
+        <Route
+          path="/homev3"
+          element={
+            <Page>
+              <HomeV3 />
             </Page>
           }
         />
---
+++ src/screens/HomeV3/HomeV3.js
@@ -0,0 +1,11 @@
+import Hero from "./Hero";
+
+const HomeV3 = () => {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+};
+
+export default HomeV3;
