c31a5a1b3d4ea47b3b5bace554321c035402b598
“Angel <angelCL0@outlook.com>
2022-05-28 08:56:18 -0400

    new

--- src/screens/Styleguide/Hero/Hero.js
+++ src/screens/Styleguide/Hero/Hero.js
@@ -1,11 +1,38 @@
 import styles from "./hero.module.css";
 import cn from "classnames";
 
+const sizes = [
+  {
+    className: "hero",
+    name: "Hero",
+    size: "96/96",
+  },
+  {
+    className: "h1",
+    name: "Heading 1",
+    size: "64/64",
+  },
+  {
+    className: "h2",
+    name: "Heading 2",
+    size: "48/56",
+  },
+];
+
 const Hero = () => {
   return (
     <div className={cn("section")}>
       <div className={cn("container", styles.container)}>
-        <h1>Hero</h1>
+        <h4 className={cn("body-bold")}>Typography | Outfit</h4>
+
+        {sizes.map((item, index) => (
+          <div className={styles.wrapper}>
+            <h4 key={index} className={cn(item.className)}>
+              {item.name}
+            </h4>
+            <p className={cn("body-2")}>{item.size}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
